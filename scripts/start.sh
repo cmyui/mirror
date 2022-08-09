b501759ecefa996c6427c16f30b09e3b2e300414
cmyui <cmyuiosu@gmail.com>
2022-08-09 18:57:32 -0400

    fix: use no timeout for tcp health checks

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export WAITFORIT_TIMEOUT=0
 wait-for-it $WRITE_DB_HOST:$WRITE_DB_PORT
 wait-for-it $ELASTIC_HOST:$ELASTIC_PORT
 # wait-for-it $REDIS_HOST:$REDIS_PORT
