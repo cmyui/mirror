ebfc72b60257bcf03fed9110abdd07850f95e8cf
cmyui <cmyuiosu@gmail.com>
2022-08-09 19:29:47 -0400

    refactor: remove cheesegull-related code
    decided after all, i'm not going to implement their spec, but rather our own (probably very close to osu!'s)

--- mount/app/enums/ranked_statuses.py
+++ mount/app/enums/ranked_statuses.py
@@ -1,16 +1,6 @@
 from __future__ import annotations
 
 
-class CheesegullRankedStatus:
-    NOT_SUBMITTED = -1
-    PENDING = 0
-    UPDATE_AVAILABLE = 1
-    RANKED = 2
-    APPROVED = 3
-    QUALIFIED = 4
-    LOVED = 5
-
-
 class OsuAPIRankedStatus:
     GRAVEYARD = -2
     WORK_IN_PROGRESS = -1
@@ -34,18 +24,6 @@ class OsuDirectStatus:
 
 class MirrorRankedStatus(OsuAPIRankedStatus):
     ALL = -3
-
-
-def osu_api_to_cheesegull_status(osu_api_status: int) -> int:
-    return {
-        OsuAPIRankedStatus.GRAVEYARD: CheesegullRankedStatus.PENDING,
-        OsuAPIRankedStatus.WORK_IN_PROGRESS: CheesegullRankedStatus.PENDING,
-        OsuAPIRankedStatus.PENDING: CheesegullRankedStatus.PENDING,
-        OsuAPIRankedStatus.RANKED: CheesegullRankedStatus.RANKED,
-        OsuAPIRankedStatus.APPROVED: CheesegullRankedStatus.APPROVED,
-        OsuAPIRankedStatus.QUALIFIED: CheesegullRankedStatus.QUALIFIED,
-        OsuAPIRankedStatus.LOVED: CheesegullRankedStatus.LOVED,
-    }[osu_api_status]
 
 
 def osu_api_to_osu_direct_status(osu_api_status: int) -> int:
--- mount/app/models/beatmap_sets.py
+++ mount/app/models/beatmap_sets.py
@@ -1,42 +1,6 @@
 from __future__ import annotations
 
-from datetime import datetime
-from typing import TYPE_CHECKING
-from typing import TypedDict
-
 from pydantic import BaseModel
-
-from app.enums.ranked_statuses import (
-    osu_api_to_cheesegull_status,
-)
-from app.models.beatmaps import Beatmap  # TODO: fuck
-
-if TYPE_CHECKING:
-    from app.models.beatmaps import BeatmapCheesegull
-
-
-class BeatmapSetCheesegull(TypedDict):
-    """\
-    A model representing a beatmap set from a cheesegull API.
-
-    https://docs.ripple.moe/docs/cheesegull/cheesegull-api
-    """
-
-    SetID: int  # ": 1668433,
-    ChildrenBeatmaps: list[BeatmapCheesegull]  # ": [],
-    RankedStatus: int  # ": -2,
-    LastUpdate: datetime  # ": "2022-01-08T02:40:21Z",
-    LastChecked: datetime  # ": "2022-03-24T00:40:17Z",
-    Artist: str  # ": "ABCDIsStuff",
-    Title: str  # ": "Camel Man Speed",
-    Creator: str  # ": "Takalaka1",
-    Source: str  # ": "",
-    Tags: str  # ": "",
-    HasVideo: int  # ": 0,
-    Genre: int  # ": 1,
-    Language: int  # ": 1,
-    Favourites: int  # ": 0,
-    StarRating: float  # ": 0,
 
 
 class BeatmapSet(BaseModel):
@@ -60,59 +24,4 @@ class BeatmapSet(BaseModel):
     # language: int
     favourites: int
     star_rating: float  # float?
-    beatmaps: list[Beatmap]
-
-    def cheesegull_format(self) -> BeatmapSetCheesegull:
-        """Convert the beatmap set to cheesegull API format."""
-        first_beatmap = self.beatmaps[0]  # is this what other mirrors do?
-
-        return {
-            "SetID": self.id,
-            "ChildrenBeatmaps": [b.cheesegull_format() for b in self.beatmaps],
-            "RankedStatus": osu_api_to_cheesegull_status(first_beatmap.approved),
-            "LastUpdate": first_beatmap.last_update,
-            "LastChecked": datetime.now(),  # TODO
-            "Artist": first_beatmap.artist,
-            "Title": first_beatmap.title,
-            "Creator": first_beatmap.creator,
-            "Source": first_beatmap.source,
-            "Tags": first_beatmap.tags,
-            "HasVideo": first_beatmap.video,
-            "Genre": first_beatmap.genre_id,
-            "Language": first_beatmap.language_id,
-            "Favourites": first_beatmap.favourite_count,
-            "StarRating": first_beatmap.rating,
-        }
-
-    def osu_direct_format(self) -> bytes:
-        """Convert the beatmap set to osu!direct format."""
-        # create a list of difficult names sorted by difficulty
-        # '[5.19⭐] Insane {cs: 6.0 / od: 7.0 / ar: 7.0 / hp: 2.0}@0'
-        sorted_beatmaps = sorted(self.beatmaps, key=lambda m: m.difficultyrating)
-        diffs_str = ",".join(
-            [
-                (
-                    "[{difficultyrating:.2f}⭐] {version} "
-                    "{{cs: {diff_size} / od: {diff_overall} / ar: {diff_approach} / hp: {diff_drain}}}@{mode}"
-                ).format(**row.__dict__)
-                for row in sorted_beatmaps
-            ],
-        )
-
-        first_beatmap = sorted_beatmaps[0]
-
-        # b'1141.osz|FAIRY FORE|Vivid|Hitoshirenu Shourai|1|10.0|2007-11-01 05:09:15|141|0|False|0|0|0|[5.19⭐] Insane {cs: 6.0 / od: 7.0 / ar: 7.0 / hp: 2.0}@0'
-        return (
-            (
-                "{beatmapset_id}.osz|{artist}|{title}|{creator}|"
-                "{approved}|10.0|{last_update}|{beatmapset_id}|"
-                "0|{video}|0|0|0|{diffs}\n"  # 0s are threadid, has_story,
-                # filesize, filesize_novid.
-            )
-            .format(
-                **self.__dict__,
-                **first_beatmap.__dict__,
-                diffs=diffs_str,
-            )
-            .encode()
-        )
+    # beatmaps: list[Beatmap]
