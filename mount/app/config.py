cf67d124031175882730c1da19c45e4d3a97dc54
cmyui <cmyuiosu@gmail.com>
2022-08-10 03:45:44 -0400

    chore: remove code related to sql database
    we only use elasticsearch & i don't see this changing any time soon

--- mount/app/config.py
+++ mount/app/config.py
@@ -15,33 +15,6 @@ APP_PORT = config.get("APP_PORT", cast=int)
 # https://docs.python.org/3/library/logging.html#levels
 LOG_LEVEL = config.get("LOG_LEVEL", cast=int)
 
-# database
-READ_DB_USER = config.get("READ_DB_USER")
-READ_DB_PASS = config.get("READ_DB_PASS")
-READ_DB_HOST = config.get("READ_DB_HOST")
-READ_DB_PORT = config.get("READ_DB_PORT", cast=int)
-READ_DB_NAME = config.get("READ_DB_NAME")
-READ_DB_DSN = "postgresql://{}:{}@{}:{}/{}?sslmode=prefer".format(
-    READ_DB_USER,
-    READ_DB_PASS,
-    READ_DB_HOST,
-    READ_DB_PORT,
-    READ_DB_NAME,
-)
-
-WRITE_DB_USER = config.get("WRITE_DB_USER")
-WRITE_DB_PASS = config.get("WRITE_DB_PASS")
-WRITE_DB_HOST = config.get("WRITE_DB_HOST")
-WRITE_DB_PORT = config.get("WRITE_DB_PORT", cast=int)
-WRITE_DB_NAME = config.get("WRITE_DB_NAME")
-WRITE__DB_DSN = "postgresql://{}:{}@{}:{}/{}?sslmode=prefer".format(
-    WRITE_DB_USER,
-    WRITE_DB_PASS,
-    WRITE_DB_HOST,
-    WRITE_DB_PORT,
-    WRITE_DB_NAME,
-)
-
 # elasticsearch
 ELASTIC_HOST = config.get("ELASTIC_HOST")
 ELASTIC_PORT = config.get("ELASTIC_PORT")
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# wait for connected services to be ready
 export WAITFORIT_TIMEOUT=0
-wait-for-it $WRITE_DB_HOST:$WRITE_DB_PORT
 wait-for-it $ELASTIC_HOST:$ELASTIC_PORT
-# wait-for-it $REDIS_HOST:$REDIS_PORT
-# wait-for-it $RABBITMQ_HOST:$RABBITMQ_PORT
 
 case $APP_COMPONENT in
     "tests")
