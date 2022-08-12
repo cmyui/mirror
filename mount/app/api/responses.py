d178ba7924fe728db42262c5e0f0da015384e3c9
cmyui <cmyuiosu@gmail.com>
2022-08-11 22:43:19 -0400

    refactor: osu!direct response formatting

--- mount/app/api/responses.py
+++ mount/app/api/responses.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 from typing import Any
+from typing import Mapping
 
 from fastapi import status
 from fastapi.responses import ORJSONResponse
+from fastapi.responses import Response
 
 
 def success(data: Any) -> ORJSONResponse:
@@ -11,6 +13,60 @@ def success(data: Any) -> ORJSONResponse:
         content={"status": "success", "data": data},
         status_code=status.HTTP_200_OK,
     )
+
+
+def _format_beatmap_set_osu_direct(beatmap_set_data: Mapping[str, Any]) -> bytes:
+    difficulties_string = ",".join(
+        [
+            (
+                "[{difficulty_rating:.2f}⭐] {version} "
+                "{{cs: {cs} / od: {accuracy} / ar: {ar} / hp: {drain}}}@{mode_int}"
+            ).format(**beatmap)
+            # TODO: sort beatmaps by difficulty?
+            for beatmap in beatmap_set_data["beatmaps"]
+        ],
+    )
+
+    return (
+        (
+            "{id}.osz|{artist}|{title}|{creator}|"
+            "{ranked}|10.0|{last_updated}|{id}|"
+            "0|{video}|0|0|0|{difficulties_string}\n"  # 0s are threadid, has_story,
+            # filesize, filesize_novid.
+        )
+        .format(**beatmap_set_data, difficulties_string=difficulties_string)
+        .encode()
+    )
+
+
+def osu_direct(beatmapsets: list[Mapping[str, Any]]) -> Response:
+    # special case - return binary data
+    resp_data = bytearray()
+    beatmapset_count = 0
+
+    for beatmapset_data in beatmapsets:
+        difficulties_string = ",".join(
+            [
+                (
+                    "[{difficulty_rating:.2f}⭐] {version} "
+                    "{{cs: {cs} / od: {accuracy} / ar: {ar} / hp: {drain}}}@{mode_int}"
+                ).format(**beatmap)
+                # TODO: sort beatmaps by difficulty?
+                for beatmap in beatmapset_data["beatmaps"]
+            ],
+        )
+
+        beatmapset_string = (
+            "{id}.osz|{artist}|{title}|{creator}|"
+            "{ranked}|10.0|{last_updated}|{id}|"
+            "0|{video}|0|0|0|{difficulties_string}\n"  # 0s are threadid, has_story,
+            # filesize, filesize_novid.
+        ).format(**beatmapset_data, difficulties_string=difficulties_string)
+
+        resp_data.extend(beatmapset_string.encode())
+        beatmapset_count += 1
+
+    return Response(content=f"{beatmapset_count}\n".encode() + bytes(resp_data))
 
 
 def error(status_code: int, message: str) -> ORJSONResponse:
--- mount/app/api/rest/v1/beatmapsets.py
+++ mount/app/api/rest/v1/beatmapsets.py
@@ -1,45 +1,16 @@
 from __future__ import annotations
 
-from typing import Any
-from typing import Mapping
-
 from app.api import responses
 from app.enums.gamemodes import GameMode
 from app.enums.ranked_statuses import OsuAPIRankedStatus
 from app.usecases import beatmapsets
 from fastapi import APIRouter
 from fastapi.param_functions import Query
-from fastapi.responses import Response
 
 router = APIRouter()
 
 
 # TODO: response_model
-
-
-# TODO: where should this live?
-def osu_direct_format(beatmap_set_data: Mapping[str, Any]) -> bytes:
-    difficulties_string = ",".join(
-        [
-            (
-                "[{difficulty_rating:.2f}⭐] {version} "
-                "{{cs: {cs} / od: {accuracy} / ar: {ar} / hp: {drain}}}@{mode_int}"
-            ).format(**beatmap)
-            # TODO: sort beatmaps by difficulty?
-            for beatmap in beatmap_set_data["beatmaps"]
-        ],
-    )
-
-    return (
-        (
-            "{id}.osz|{artist}|{title}|{creator}|"
-            "{ranked}|10.0|{last_updated}|{id}|"
-            "0|{video}|0|0|0|{difficulties_string}\n"  # 0s are threadid, has_story,
-            # filesize, filesize_novid.
-        )
-        .format(**beatmap_set_data, difficulties_string=difficulties_string)
-        .encode()
-    )
 
 
 @router.get("/beatmapsets/search")
@@ -53,17 +24,8 @@ async def get_beatmapset_search(
     osu_direct: bool = False,
 ):
     search_results = await beatmapsets.search(query, amount, offset, mode, status)
-
     if osu_direct:
-        # special case - return binary data
-        resp_data = bytearray()
-        beatmapset_count = 0
-
-        for beatmap_set_data in search_results:
-            resp_data.extend(osu_direct_format(beatmap_set_data))
-            beatmapset_count += 1
-
-        return Response(content=f"{beatmapset_count}\n".encode() + bytes(resp_data))
+        return responses.osu_direct(search_results)
 
     return responses.success(search_results)
 
